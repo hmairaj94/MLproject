cddfc8a32c9935ef075ea11ba9e7ad842400bb99
hmairaj94 <hmairaj94@gmail.com>
2024-08-06 18:26:38 +0530

    Data transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,20 @@
+import os
+from signal import raise_signal
+import sys
+import numpy as np
 import pandas as pd
+import dill
 
-# Sample dataset
-data = {
-    'Transaction_ID': [1, 2, 3],
-    'Date': ['2023-08-01', '2023-08-02', '2023-08-03'],
-    'Product': ['Product A', 'Product B', 'Product C'],
-    'Quantity': [5, 10, 2],
-    'Price_per_Unit': [100, 50, 200]
-}
+from src.exception import CustomException
 
-# Create a pandas DataFrame
-df = pd.DataFrame(data)
+def save_object(file_path,obj):
+    try:
+        dir_path= os.path.dirname(file_path)
 
-# Define the function to calculate total revenue
-def calculate_total_revenue(df):
-    df['Total_Price'] = df['Quantity'] * df['Price_per_Unit']
-    total_revenue = df['Total_Price'].sum()
-    return total_revenue
+        os.makedirs(dir_path,exist_ok=True)
 
-# Calculate the total revenue
-total_revenue = calculate_total_revenue(df)
-print(f"The total revenue is: {total_revenue}")
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+                    
+    except Exception as e:
+        raise CustomException(e,sys)
